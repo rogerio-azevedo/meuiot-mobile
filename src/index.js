42beefd78ec6ca99dc30aababd2fbb462b8589bc
Rogerio <rogerio.francis@gmail.com>
2019-12-27 19:00:25 -0400

    Deploy CodePush

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+
+import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
 import { StatusBar } from 'react-native';
 import './config/ReactotronConfig';
@@ -8,13 +11,41 @@ import './config/ReactotronConfig';
 import { store, persistor } from './store';
 import App from './App';
 
-export default function Index() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <StatusBar barStyle="light-content" backgroundColor="#159957" />
-        <App />
-      </PersistGate>
-    </Provider>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init('eec43c23-7d23-4686-87c8-b8a2a1976dfd');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => {};
+
+  onOpened = notification => {};
+
+  onIds = id => {};
+
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#159957" />
+          <App />
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
+
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(App);
